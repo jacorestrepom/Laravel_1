9cc179e07d3ce2bdd0e1cbcad3ba34af60cce89c
pollorest <jrestrep32@eafit.edu.co>
2025-02-25 19:32:49 -0500

    Tutorial 3 de Laravel completado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
                     <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
                     <a class="nav-link active" href="{{ route('home.about') }}">About</a>
                     <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+                    <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/about', function () {
     $data1 = 'About us - Online Store';
     $data2 = 'About us';
     $description = 'This is an about page ...';
-    $author = 'Developed by: Your Name';
+    $author = 'Developed by: Jacobo';
 
     return view('home.about')->with('title', $data1)
         ->with('subtitle', $data2)
@@ -18,3 +18,10 @@ Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('p
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name('image.index');
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name('image.save');
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name('imagenotdi.index');
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name('imagenotdi.save');
